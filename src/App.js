e87f3e2a403e75e253542cebbd92341cc1a776af
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2024-11-20 23:30:39 +1100

    Added trees

--- src/App.js
+++ src/App.js
@@ -4,31 +4,15 @@ import { Environment, OrbitControls } from '@react-three/drei';
 import { Bloom, EffectComposer} from '@react-three/postprocessing';
 import * as THREE from 'three';
 import Campfire from './components/Campfire/Campfire';
+import SceneCamp from './components/Campfire/Scene';
+
 
 function App() {
   return (
     <div style={{ height: '100vh', background:'black' }}>
       <Canvas shadows={true}>
-        {/* <Environment preset={'forest'}/> */}
-    
         <OrbitControls />
-        <directionalLight 
-          position={[5, 5, 5]} 
-          intensity={1} // Lower intensity for soft moonlight
-          color={new THREE.Color(0xaaaaaa)} // Pale white-blue color for moonlight
-          castShadow
-        />
-        
-        <Campfire />
-
-        <mesh position={[0, -2.5, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-          <planeGeometry args={[50, 50]} />
-          <meshStandardMaterial color={new THREE.Color('rgb(120, 68, 11)')} />
-        </mesh>
-
-        {/* <EffectComposer>
-          <Bloom intensity={40} luminanceThreshold={1}  height={300} />
-        </EffectComposer> */}
+        <SceneCamp/>
       </Canvas>
     </div>
   );
--- src/components/Campfire/Campfire.js
+++ src/components/Campfire/Campfire.js
@@ -1,99 +1,107 @@
 import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-import { Log } from './Log';
+import {  useControls } from 'leva'; // For UI controls
 import FireLog from './FireLog';
+import * as THREE from 'three';
 
-let options = {
-  color: [213, 100, 14],
-};
-
-const fragflame = `
-    varying vec2 vUv;
-    uniform sampler2D noise;
-    uniform vec3 color4;
-    uniform float time;
-    varying vec3 vNormal;
-
-    vec3 rgbcol(vec3 col) {
-        return vec3(col.r/255.0,col.g/255.0,col.b/255.0);
-    }
-
-      
-   void main() {
-    vec3 noisetex = texture2D(noise, mod(1. * vec2(vUv.y + time * 2., vUv.x - time * 1.), 1.)).rgb;
-    vec3 flameColor = vec3(1.0, 0.5, 0.2); 
-    gl_FragColor = vec4(noisetex.r * flameColor, noisetex.r);
-
-    if (gl_FragColor.r >= 0.44) {
-        gl_FragColor = vec4(rgbcol(color4), gl_FragColor.r);
-    } else {
-        gl_FragColor = vec4(0.0);
-    }
-
-    gl_FragColor *= vec4(smoothstep(0.2, 0.628, vUv.y));
-    gl_FragColor.rgb *= 2.0; // Boost brightness for bloom effect
-}
-
-
-`;
-
-const vertflame = `
-    varying vec2 vUv;
-    varying vec3 camPos;
-    varying vec3 vNormal;
-    uniform sampler2D noise;
-    uniform float time;
-
-    void main() {
-        vUv = uv;
-        camPos = cameraPosition;
-        vNormal = normal;
-        vec3 pos = vec3(position.x/1.,position.y,position.z/1.);
-        vec3 noisetex = texture2D(noise,mod(1.*vec2(vUv.y+time*2.,vUv.x - time*1.),1.)).rgb;
-        if(pos.y >= 1.87){
-            pos = vec3(
-                position.x*(sin((position.y - 0.64)*1.27)-0.12),
-                position.y,
-                position.z*(sin((position.y - 0.64)*1.27)-0.12));
-        } else{
-            pos = vec3(
-                position.x*(sin((position.y/2. -  .01)*.11)+0.79),
-                position.y,
-                position.z*(sin((position.y/2. -  .01)*.11)+0.79));
-        }
-        pos.xz *= noisetex.r;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
-    }
-`;
 
 export default function Campfire() {
-  const meshRef = useRef();
-  const shaderRef = useRef();
+
   const fireLightRef = useRef();
 
+
+  // const { fireRotationX, firePositionY, firePositionZ } = useControls({
+  //   fireRotationX: { value: -2.71, min: -Math.PI, max: Math.PI, step: 0.01 },
+  //   firePositionY: { value: 0, min: -3, max: 3, step: 0.01 },
+  //   firePositionZ: { value: 0, min: -3, max: 3, step: 0.01 }
+  // });
+
+
+
   useFrame(({ clock }) => {
-    if (shaderRef.current) {
-      shaderRef.current.uniforms.time.value = clock.getElapsedTime() / 4;
-    }
     if (fireLightRef.current) {
-      const time = clock.getElapsedTime();
+      const time = fireLightRef.current.randomSeed + clock.getElapsedTime() *2;
       const sineFunction =
         Math.sin(time) * Math.sin(time) +
         Math.sin(2 * time) -
-        Math.cos(time) * Math.sin((3 * time) / 2) +
-        Math.random() * 0.2 -
-        0.1;
+        Math.cos(time) * Math.sin((3 * time) / 2);
 
-      const flicker = Math.abs(sineFunction) * 0.5 + 0.5;
-      fireLightRef.current.intensity = flicker * 50.5;
+      const flicker = Math.abs(sineFunction) * 0.2 + 0.5;
+      fireLightRef.current.intensity = flicker * 1;
     }
   });
 
   return (
-    <>
-      <FireLog position={[0,0,0]} logPosition={[-1, -1, -1]} logRotation={[Math.PI/1.5, 0,0]}  />
-      <FireLog position={[0,0,-3]} rotation={[0,Math.PI/1,0]} logPosition={[-1, -1, -1]} logRotation={[Math.PI/1.5, 0,0]}  />
-    </>
+    <group>
+       <pointLight
+          position={[0, 0, 0]}
+          intensity={1}
+          color={new THREE.Color('#fefa9b')}
+          decay={0.1}
+          distance={50}
+          castShadow={true}
+          power={90}
+          randomSeed = {Math.random()}
+        /> 
+        {/* <pointLight
+          ref={fireLightRef}
+          position={[0, 0, 0]}
+          intensity={2}
+          color={new THREE.Color('#fefa9b')}
+          decay={0.1}
+          distance={10}
+          power={70}
+          castShadow={true}
+          randomSeed = {Math.random()}
+        />  */}
+      <FireLog
+        rotation={[-0.5, 0, 0]}
+        position={[0, 0 ,0]}
+        logScale={[1, 0.5, 1]}
+        clip={{x: 0, y:-0.2, z:0.8}}
+        fireRotation={[-2.71, 0 , 0]}
+        firePosition= {[0, 0.49, -0.13 ]}
+      />
+      <FireLog
+        rotation={[-1.5, -0.4, -1.1]}
+        position={[0.05, -0.24 , -0.3]}
+        logScale={[1, 0.5, 1]}
+        clip={{x: 0, y:0, z:0}}
+        fireRotation={[-1.9, 0 , 0]}
+        firePosition= {[0, 0.20, 0.34 ]}
+      />
+      <FireLog
+        rotation={[-1.53, -0.2, 0.35]}
+        position={[0.32, -0.65 , -0.4]}
+        logScale={[1, 0.5, 1]}
+        clip={{x: 0, y:0, z:0}}
+        fireRotation={[-1.6, 0 , 0]}
+        firePosition= {[0, 0.08, 0.50 ]}
+      />
+      <FireLog
+        rotation={[1.33, -2.39, 1.07]}
+        position={[0.06, 0 , -0.58]}
+        logScale={[0.6, 0.6, 0.6]}
+        clip={{x: 0, y:-0.5, z:1}}
+        fireRotation={[-2.34, 0 , 0]}
+        firePosition= {[0, 0.5, 0.01 ]}
+      />
+      <FireLog
+        rotation={[-2.15, 0.55, 2.31]}
+        position={[0.15, 0.01, -0.92]}
+        logScale={[1, 0.5, 1]}
+        clip={{x: 0, y:-0.2, z:0.8}}
+        fireRotation={[-2.2, 0 , 0]}
+        firePosition= {[0, 0.43, 0.09 ]}
+      />
+      <FireLog
+        rotation={[-0.86, 0.78, 0.77]}
+        position={[0.66, -0.09, -0.22]}
+        logScale={[0.8, 0.4, 0.8]}
+        clip={{x: 0, y:-0.2, z:0.8}}
+        fireRotation={[-2.5, 0 , 0]}
+        firePosition= {[0, 0.32, -0.07 ]}
+      />
+    </group>
   );
 }
