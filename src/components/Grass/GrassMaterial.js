040e263cdcb1a0f790399f9e45f85b5cc8bc13f2
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2024-11-25 04:12:21 +1100

    Grass Shader updated

--- src/components/Grass/GrassMaterial.js
+++ src/components/Grass/GrassMaterial.js
@@ -17,16 +17,19 @@ const GrassMaterial = shaderMaterial(
       attribute float halfRootAngleSin;
       attribute float halfRootAngleCos;
       attribute float stretch;
+      attribute float shade;
       uniform float time;
       uniform float bladeHeight;
       varying vec2 vUv;
       varying float frc;
+      varying float vShade;
       
       //WEBGL-NOISE FROM https://github.com/stegu/webgl-noise
       //Description : Array and textureless GLSL 2D simplex noise function. Author : Ian McEwan, Ashima Arts. Maintainer : stegu Lastmod : 20110822 (ijm) License : Copyright (C) 2011 Ashima Arts. All rights reserved. Distributed under the MIT License. See LICENSE file. https://github.com/ashima/webgl-noise https://github.com/stegu/webgl-noise      
       vec3 mod289(vec3 x) {return x - floor(x * (1.0 / 289.0)) * 289.0;} vec2 mod289(vec2 x) {return x - floor(x * (1.0 / 289.0)) * 289.0;} vec3 permute(vec3 x) {return mod289(((x*34.0)+1.0)*x);} float snoise(vec2 v){const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439); vec2 i  = floor(v + dot(v, C.yy) ); vec2 x0 = v -   i + dot(i, C.xx); vec2 i1; i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0); vec4 x12 = x0.xyxy + C.xxzz; x12.xy -= i1; i = mod289(i); vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 )); vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0); m = m*m ; m = m*m ; vec3 x = 2.0 * fract(p * C.www) - 1.0; vec3 h = abs(x) - 0.5; vec3 ox = floor(x + 0.5); vec3 a0 = x - ox; m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h ); vec3 g; g.x  = a0.x  * x0.x  + h.x  * x0.y; g.yz = a0.yz * x12.xz + h.yz * x12.yw; return 130.0 * dot(m, g);}
       //END NOISE
       
+
       //https://www.geeks3d.com/20141201/how-to-rotate-a-vertex-by-a-quaternion-in-glsl/
       vec3 rotateVectorByQuaternion( vec3 v, vec4 q){
         return 2.0 * cross(q.xyz, v * q.w + cross(q.xyz, v)) + v;
@@ -82,7 +85,7 @@ const GrassMaterial = shaderMaterial(
         direction = slerp(direction, orientation, frc);
         vec3 vPosition = vec3(position.x, position.y + position.y * stretch, position.z);
         vPosition = rotateVectorByQuaternion(vPosition, direction);
-      
+        vShade = shade;
        //Apply wind
        float halfAngle = noise * 0.15;
         vPosition = rotateVectorByQuaternion(vPosition, normalize(vec4(sin(halfAngle), 0.0, -sin(halfAngle), cos(halfAngle))));
@@ -99,6 +102,7 @@ const GrassMaterial = shaderMaterial(
       uniform vec3 bottomColor;
       varying vec2 vUv;
       varying float frc;
+      varying float vShade;
       
       void main() {
         //Get transparency information from alpha map
@@ -111,6 +115,7 @@ const GrassMaterial = shaderMaterial(
         col = mix(vec4(tipColor, 1.0), col, frc);
         //Add a shadow towards root
         col = mix(vec4(bottomColor, 1.0), col, frc);
+        col.rgb *= vShade;
         gl_FragColor = col;
 
         #include <tonemapping_fragment>
